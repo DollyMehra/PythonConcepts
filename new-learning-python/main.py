357b60e515996d835ac0f5a8fbec6b78cc557b8d
dollymehra <dollymehra1029@gmail.com>
2023-02-28 16:02:25 +0530

    test

--- new-learning-python/main.py
+++ new-learning-python/main.py
@@ -1,14 +1,26 @@
-print("Hello dolly")
-print(2023)
+# print("Hello dolly")
+# print(2023)
 
-print("learning is\n important") #\n is for escape sequence
+# print("learning is\n important") #\n is for escape sequence
 
-v1 = "one" #variables
-v2 = True
-v3 = None
-v4 = 5
+# v1 = "one" #variables
+# v2 = True
+# v3 = None
+# v4 = 5
 
-print (v1)
-print (v2)
-print (v3)
-print (v4)
+# print (v1)
+# print (v2)
+# print (v3)
+# print (v4)
+
+
+first_name = "dolly"
+last_name = "mehra"
+ice_cream = ". Would you like to eat an \"icecream\" ?"
+
+print("hello" , first_name , last_name , ice_cream)
+print(first_name[0]) #indexing 
+
+for i in last_name:
+    print(i)
+    
