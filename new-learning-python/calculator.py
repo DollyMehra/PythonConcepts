09c9f5f5fdefe611a06bd680ab58c309ed4b3f10
dollymehra <dollymehra1029@gmail.com>
2023-02-28 12:10:01 +0530

    calculator

---
+++ new-learning-python/calculator.py
@@ -0,0 +1,10 @@
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+print("addition of a + b is :" , a + b )
+print("subraction of a - b is :" , a - b )
+print("multiplication of a * b is :" , a * b )
+print("division of a / b is :" , a / b )
+print("modulo of a // b is :" , a // b )
+print("exponantial of a**b is :" , a**b )
+
+
