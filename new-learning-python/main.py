ffe055328d255a1dc0d59a3fac44391e22d75077
dollymehra <dollymehra1029@gmail.com>
2023-02-28 11:47:20 +0530

    test

--- new-learning-python/main.py
+++ new-learning-python/main.py
@@ -1,2 +1,14 @@
 print("Hello dolly")
 print(2023)
+
+print("learning is\n important") #\n is for escape sequence
+
+v1 = "one" #variables
+v2 = True
+v3 = None
+v4 = 5
+
+print (v1)
+print (v2)
+print (v3)
+print (v4)
