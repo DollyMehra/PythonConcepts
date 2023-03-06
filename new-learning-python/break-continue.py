d1c5948f5d98856d3b39f2dce26c5c835b7ebd9b
dollymehra <dollymehra1029@gmail.com>
2023-03-06 18:48:07 +0530

    test

---
+++ new-learning-python/break-continue.py
@@ -0,0 +1,27 @@
+# items = ["a","b","c","d","e"]
+# for i in items:
+#     if i == "c":
+#         break
+#     print(i)
+
+
+# items = ["a","b","c","d","e"]
+# for i in items:
+#     if i == "c":
+#         continue
+#     print(i)
+
+
+# for i in range(10):
+#     if i == 7:
+#         break
+#     print("5" ,"X", i+1 , "=" , 5*(i+1))
+
+
+# for i in range(10):
+#     if i == 8:
+#      continue
+#     print("2" ,"X", i+1 , "=" , 2*(i+1))
+
+  
+    #print("hello")
---
+++ new-learning-python/while_loop.py
@@ -0,0 +1,14 @@
+# count = 5
+# while (count > 0):
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside")
+        
+# i = 1
+
+# while True:
+#     print(i)
+#     i = i + 1
+#     if(i > 3):
+#         break
