e7fcff8f63d8b4e9d8216d60a81d46b050d8a856
dollymehra <dollymehra1029@gmail.com>
2023-03-20 21:01:58 +0530

    recursion

--- new-learning-python/recursion.py
+++ new-learning-python/recursion.py
@@ -2,22 +2,24 @@
 '''
 #
 # 5! = 5 * 4 * 3 * 2 * 1
-print the factorial of number
+#print the factorial of number
 
+n = int(input("Enter the number: "))
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
       return n * factorial(n-1)
     
-print(factorial(3))    
+print(factorial(n))    
 '''
 
-'''
+"""
 #print a fibonacci series
 # 0 1 1 2 3 5 8
 # 0 1 2 3 4 5 6 
 
+n = int(input("Enter the number: "))
 def fibonacci(n):
     if n == 0:
         return 0
@@ -27,8 +29,8 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-print(fibonacci(0))  
-print(fibonacci(1)) 
-print(fibonacci(4))   
-        
-'''
+# print(fibonacci(0))  
+# print(fibonacci(1)) 
+# print(fibonacci(4))
+print(fibonacci(n))   
+"""
